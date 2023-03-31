b95a9f2fa26ae8ea6bd754d2ce3756ea98463475
luxlorys <bossbyrcev221@gmail.com>
2023-03-31 15:35:04 +0300

    log in process created

--- app/src/main/java/com/example/testlogandregapp/MainActivity.kt
+++ app/src/main/java/com/example/testlogandregapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.testlogandregapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testlogandregapp.databinding.ActivityMainBinding
+import com.example.testlogandregapp.db.AppUserDatabase
+import com.example.testlogandregapp.user.User
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appDb: AppUserDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        appDb = AppUserDatabase.getDatabase(this)
+
+        binding.regBtn.setOnClickListener{
+            startActivity(Intent(applicationContext, RegisterActivity::class.java))
+        }
+
+        binding.logInBtn.setOnClickListener { login() }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun login() {
+        val name = binding.username.text.toString()
+        val password = binding.userPassword.text.toString()
+
+        if (name.isNotEmpty() && password.isNotEmpty()) {
+            GlobalScope.launch {
+                val user: User? = appDb.studentDao().findUserByUsernameAndPassword(name, password)
+
+                withContext(Dispatchers.Main) {
+                    if (user != null) {
+
+                        val intent = Intent(applicationContext, UserActivity::class.java)
+                        intent.putExtra("name", name)
+
+                        binding.logInBtn.setOnClickListener {
+                            startActivity(intent)
+                        }
+                    } else {
+                        Toast.makeText(this@MainActivity, "User not found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/testlogandregapp/UserActivity.kt
+++ app/src/main/java/com/example/testlogandregapp/UserActivity.kt
@@ -2,10 +2,17 @@ package com.example.testlogandregapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testlogandregapp.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.username.text = intent.getStringExtra("name")
     }
 }
