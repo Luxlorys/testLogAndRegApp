fb7b635b34be8d05332a8bef3791a1d5e24c71b7
luxlorys <bossbyrcev221@gmail.com>
2023-03-31 15:21:10 +0300

    user dao created

--- app/src/main/java/com/example/testlogandregapp/user/UserDao.kt
+++ app/src/main/java/com/example/testlogandregapp/user/UserDao.kt
@@ -1,4 +1,22 @@
 package com.example.testlogandregapp.user
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(user: User)
+
+    @Query("SELECT * FROM user_table WHERE username = :username AND user_password = :password")
+    fun findUserByUsernameAndPassword(username: String, password: String): User?
+
+    @Query("SELECT * FROM user_table WHERE user_phone_number = :phone")
+    fun phoneAlreadyExists(phone: String): User?
+
+    @Query("SELECT * FROM user_table WHERE username = :username")
+    fun usernameAlreadyExists(username: String): User?
 }
