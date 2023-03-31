75c02253384d2298b1655d5f3b2806771fdc9a78
luxlorys <bossbyrcev221@gmail.com>
2023-03-31 15:29:10 +0300

    setup db object

--- app/src/main/java/com/example/testlogandregapp/db/AppUserDatabase.kt
+++ app/src/main/java/com/example/testlogandregapp/db/AppUserDatabase.kt
@@ -1,4 +1,36 @@
 package com.example.testlogandregapp.db
 
-class AppUserDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.testlogandregapp.user.User
+import com.example.testlogandregapp.user.UserDao
+
+@Database(entities = [User::class], version = 2)
+abstract class AppUserDatabase : RoomDatabase() {
+    abstract fun studentDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppUserDatabase? = null
+
+        fun getDatabase(context: Context): AppUserDatabase {
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppUserDatabase::class.java,
+                    "app_database"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
